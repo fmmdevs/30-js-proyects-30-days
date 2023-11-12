4d9d76a5555a7438c376c1e8c4df70e06b998a1d
fmmdevs <fmm.devs@gmail.com>
2023-11-12 18:44:00 +0100

    p5 almost finished

--- 5. Animated Nav/nav.js
+++ 5. Animated Nav/nav.js
@@ -5,5 +5,27 @@ menuToggle.addEventListener("click", () => {
   for (let active = 0; active < activeElements.length; active++) {
     activeElements[active].classList.toggle("active");
   }
-  console.log("yes");
+
+  console.log(menuToggle.childNodes);
+  console.log(menuToggle.childNodes[1]);
+  // otra forma de seleccionar elementos
+  const navBtnIcon = menuToggle.childNodes[1];
+  navBtnIcon.classList.toggle("fa-times");
+
+  const pageBg = document.querySelector(".main-container");
+
+  if (navBtnIcon.classList.contains("fa-times")) {
+    pageBg.style.filter = "blur(3px)";
+  } else {
+    setTimeout(function () {
+      pageBg.style.filter = "blur(0)";
+    }, 500);
+  }
+
+  const listItems = document.querySelectorAll(".main-nav-list a");
+  listItems.forEach(function (item, index) {
+    // console.log(index);
+    // console.log((item.style.transition = `all. 5s linear ${index * 0.3}s`));
+    item.style.transition = `all. 5s linear ${index * 0.3}s`;
+  });
 });
